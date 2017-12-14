03b1150c90ebedea6d56534894abada859d8588a
Patrick Lu <lightning.lu7@gmail.com>
2017-12-13 21:45:49 -0800

    new build

--- src/lib/components/Menu/tests/Menu.test.js
+++ src/lib/components/Menu/tests/Menu.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Menu from './Menu';
+import Menu from '../Menu';
 
 it('Menu renders without crashing', () => {
   const div = document.createElement('div');
--- src/lib/components/Menu/tests/Nav.test.js
+++ src/lib/components/Menu/tests/Nav.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Nav from './Nav';
+import Nav from '../components/Nav';
 
 it('Nav renders without crashing', () => {
   const div = document.createElement('div');
